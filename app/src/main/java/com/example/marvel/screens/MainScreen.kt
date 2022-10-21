915fef581c8550e8f08bc2110a4b58f30edb6721
vsls <vasilisa_001@mail.ru>
2022-10-21 09:04:45 +0600

    Изменение цвета при свайпах

--- app/src/main/java/com/example/marvel/screens/MainScreen.kt
+++ app/src/main/java/com/example/marvel/screens/MainScreen.kt
@@ -1,21 +1,29 @@
 package com.example.marvel.screens
 
+import android.graphics.BitmapFactory
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.palette.graphics.Palette
 import com.example.marvel.R
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -30,11 +38,15 @@ fun MainScreen() {
         color = MaterialTheme.colors.surface,
         contentColor = MaterialTheme.colors.surface
     ) {
+        val color = remember { mutableStateOf(Color.Gray) }
+
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+
             Spacer(modifier = Modifier.height(25.dp))
+
             Image(
                 painter = painterResource(id = R.drawable.marvel_logo),
                 contentDescription = stringResource(
@@ -43,7 +55,9 @@ fun MainScreen() {
                 modifier = Modifier.height(45.dp),
                 contentScale = ContentScale.Fit
             )
+
             Spacer(modifier = Modifier.height(25.dp))
+
             Text(
                 text = stringResource(id = R.string.choose_your_hero),
                 style = MaterialTheme.typography.h4.copy(
@@ -51,12 +65,35 @@ fun MainScreen() {
                 ),
                 color = MaterialTheme.colors.onSurface
             )
+
             Spacer(modifier = Modifier.height(25.dp))
+
             HorizontalPager(
                 count = info.size,
                 contentPadding = PaddingValues(32.dp),
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .drawBehind {
+                        rotate(degrees = 45f) {
+                            drawRect(
+                                color = color.value,
+                                topLeft=Offset(400f,100f),
+                                size = size/1f
+                            )
+                        }
+                    }
             ) { page ->
+                val hero = info[page]
+                val bitmap =
+                    BitmapFactory.decodeResource(LocalContext.current.resources, hero.drawable)
+
+                val palette = Palette.from(bitmap).generate()
+                palette.run {
+                    swatches.forEach { swatch ->
+                        color.value=Color(swatch.rgb)
+                    }
+                }
+
                 Card(
                     modifier = Modifier
                         .graphicsLayer {
@@ -73,7 +110,6 @@ fun MainScreen() {
                         .fillMaxWidth(),
                     shape = RoundedCornerShape(15.dp)
                 ) {
-                    val hero = info[page]
                     CardOfHero(hero = hero)
                 }
             }
@@ -104,6 +140,7 @@ fun CardOfHero(hero: Hero) {
             modifier = Modifier
                 .fillMaxSize()
         )
+
         Text(
             text = hero.name,
             modifier = Modifier
@@ -112,7 +149,7 @@ fun CardOfHero(hero: Hero) {
             maxLines = 1,
             style = MaterialTheme.typography.h4.copy(
                 fontWeight = FontWeight.ExtraBold
-            ),
+            )
         )
     }
 }
@@ -120,33 +157,28 @@ fun CardOfHero(hero: Hero) {
 data class Hero(
     val id: Int,
     val name: String,
-    @DrawableRes val drawable: Int,
-    val color: Color
+    @DrawableRes val drawable: Int
 )
 
 val info = listOf<Hero>(
     Hero(
         id = 0,
         name = "Thor",
-        drawable = R.drawable.thor,
-        color = Color.Black
+        drawable = R.drawable.thor
     ),
     Hero(
         id = 1,
         name = "Deadpool",
-        drawable = R.drawable.deadpool,
-        color = Color.Blue
+        drawable = R.drawable.deadpool
     ),
     Hero(
         id = 2,
         name = "Thanos",
-        drawable = R.drawable.thanos,
-        color = Color.Blue
+        drawable = R.drawable.thanos
     ),
     Hero(
         id = 3,
         name = "Deadpool",
-        drawable = R.drawable.deadpool,
-        color = Color.Blue
+        drawable = R.drawable.deadpool
     ),
 )
