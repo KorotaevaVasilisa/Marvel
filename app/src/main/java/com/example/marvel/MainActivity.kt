d1318170aab6f7887645aa2a379cdd05d01d6a19
vsls <vasilisa_001@mail.ru>
2022-11-18 01:30:50 +0600

    Добавление Hilt

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.example.marvel.navigation.AppNavHost
 import com.example.marvel.ui.theme.MarvelTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
