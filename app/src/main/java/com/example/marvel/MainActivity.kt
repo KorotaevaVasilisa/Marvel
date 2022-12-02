fe4efc969472065031146766674b1cafd0fa6ec2
vsls <vasilisa_001@mail.ru>
2022-12-02 15:24:46 +0600

    Реализация Notification

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.example.marvel.navigation.AppNavHost
 import com.example.marvel.ui.theme.MarvelTheme
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseMessaging.getInstance().subscribeToTopic("all").addOnCompleteListener {}
         setContent {
             MarvelTheme() {
                 Surface(
